6397680cf4e699ffd47d50bee9e1362d58506614
Anjana Ganesh <141896402+anjanaganesh06@users.noreply.github.com>
2025-09-27 22:27:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config:
     MYSQL_HOST = 'localhost'  # Change to your MySQL host
     MYSQL_USER = 'root'  # Change to your MySQL username
-    MYSQL_PASSWORD = 'Grape2004'
-    MYSQL_DB = 'dbmsminiproj'
+    MYSQL_PASSWORD = 'Your_password'
+    MYSQL_DB = 'Your_db'
+
